d92ef4c494a32c7e919339f58e703807aed6a98c
cabot <cabot@users.noreply.github.com>
2022-11-25 14:00:11 +0100

    Update functions.js

--- template/functions.js
+++ template/functions.js
@@ -23,36 +23,36 @@ $(function () {
 });
 
 // Show/hide password
-if ( (typeof showPassword !== "undefined") && $("[type='password']").length ) {
+if ((typeof showPassword !== "undefined") && $("[type='password']").length) {
 	let $passwordInput = $("input[type='password']"),
 		showpassBtn = ("<button type='button' class='showpass-btn fa fa-eye' data-showpass></button>");
 
-	$passwordInput.each(function() {
+	$passwordInput.each(function () {
 		$(this).after(showpassBtn);
 	});
 
-	$("[data-showpass]").click(function() {
+	$("[data-showpass]").click(function () {
 		let thisPassword = $(this).prev($passwordInput),
 			typeValue = thisPassword.attr("type") === "password" ? "text" : "password";
 
-		$(this).toggleClass("fa-eye fa-eye-slash").blur();
+		$(this).toggleClass("fa-eye fa-eye-slash");
 		thisPassword.attr("type", typeValue).toggleClass("warning");
 	});
 }
 
 // Display first username letter as default avatar in viewtopic mini-profile
 if ($(".avatar-letter").length) {
-	$(".no-avatar").each(function() {
+	$(".no-avatar").each(function () {
 		let $letterContainer = $(this).find(".avatar-letter"),
 			$usernameLink = $(this).find("[class*='username']"),
-			firstLetter = $usernameLink.text().slice(0,1);
-			
+			firstLetter = $usernameLink.text().slice(0, 1);
+
 		$letterContainer.append(firstLetter);
 	});
 }
 
 /* Animate scroll to top */
-$(window).scroll(function() {
+$(window).scroll(function () {
 	if ($(this).scrollTop() > 250) {
 		$(".scrolltop").fadeIn();
 	} else {
@@ -60,30 +60,32 @@ $(window).scroll(function() {
 	}
 });
 
-$(".scrolltop, .top").click(function() {
-	$("html, body").animate({scrollTop : 0}, "fast");
+$(".scrolltop, .top").click(function () {
+	$("html, body").animate({ scrollTop: 0 }, "fast");
 	return false;
 });
 
 // Let user change main colors
-$(function() {
-    let $root = $("html"),
-		cssVar = "--main-";
-	
-	$(".cp-list-item").each(function() {
+$(function () {
+	let $root = $("html"),
+		cssVar = "--main-",
+		cancelLang = $("#getcolors").data("cancel"),
+		confirmLang = $("#getcolors").data("confirm");
+
+	$(".cp-list-item").each(function () {
 		let $colPickItem = $(this).find("input"),
 			itemId = $colPickItem.prop("id"),
 			$buttonReset = $(this).find("button"),
 			originalColor = $("#getcolors").css(cssVar + itemId);
-	
+
 		if (localStorage.getItem("main" + itemId)) {
 			let storedColor = localStorage.getItem("main" + itemId);
 			$("#" + itemId).attr("value", storedColor);
 			$buttonReset.removeAttr("disabled");
 		} else {
 			$("#" + itemId).attr("value", originalColor);
-		}			
-	
+		}
+
 		function spectrumize(colorOption) {
 			$colPickItem.spectrum({
 				type: "color",
@@ -118,35 +120,36 @@ $(function() {
 		}
 		spectrumize();
 
-		$buttonReset.click(function() {
+		$buttonReset.click(function () {
 			$colPickItem.spectrum("destroy");
 			$root.css(cssVar + itemId, "");
-			$(this).blur().prop("disabled", true);
-			
+			$(this).prop("disabled", true);
+
 			spectrumize(originalColor);
 
 			let removeEachItem = ["main" + itemId];
 			for (key of removeEachItem) {
 				localStorage.removeItem(key);
 			}
+
 		});
 
 	});
 });
 
 /* Modal Login Window */
-$(function() {
+$(function () {
 	let $loginLink = $("#modal-login-trigger"),
 		$form = "#modal-login",
 		$phpbbWrapper = "#darkenwrapper";
 
 	if ($($form).length) {
-		$loginLink.click(function(e) {
+		$loginLink.click(function (e) {
 			e.preventDefault();
 			$($phpbbWrapper + ", " + $form).fadeIn(300);
 		});
 
-		$($phpbbWrapper + ", " + $form + " .alert_close").click(function(e) {
+		$($phpbbWrapper + ", " + $form + " .alert_close").click(function (e) {
 			e.preventDefault();
 			$($phpbbWrapper + ", " + $form).fadeOut(300);
 		});
